059f84cc6aa57381e5c616212286bf47f2c2b998
Jian Han <jianhan@mail.com>
2020-03-11 15:47:02 +1000

    Added new getClientsFromEnvs function.

--- src/clients.ts
+++ src/clients.ts
@@ -2,7 +2,25 @@ import Twitter = require("twitter");
 import { AccessTokenOptions } from "twitter";
 import S3 = require("aws-sdk/clients/s3");
 import { ClientConfiguration } from "aws-sdk/clients/s3";
+import { Environment } from "jianhan-fp-lib";
+import * as immutable from "immutable";
 
 export const getTwitterClient = (options: AccessTokenOptions): Twitter => new Twitter(options);
+
 export const getS3Client = (configs: ClientConfiguration) => new S3(configs);
 
+export const getClientsFromEnvs = (envs: immutable.Map<string, string | Environment | undefined>) => {
+    const s3 = getS3Client({
+        accessKeyId: envs.get("S3_ACCESS_KEY_ID"),
+        secretAccessKey: envs.get("S3_SECRET_ACCESS_KEY"),
+    });
+
+    const tw = getTwitterClient({
+        consumer_key: envs.get("CONSUMER_API_KEY") as string,
+        consumer_secret: envs.get("CONSUMER_API_SECRET_KEY") as string,
+        access_token_key: envs.get("ACCESS_TOKEN") as string,
+        access_token_secret: envs.get("ACCESS_SECRET") as string,
+    });
+
+    return { s3, tw };
+}
--- src/users/lookup/sync.int.test.ts
+++ src/users/lookup/sync.int.test.ts
@@ -1,13 +1,11 @@
-import S from "sanctuary"
 import { Environment, getEnvs, createLogger, LogLevel } from "jianhan-fp-lib";
 import { Logger } from "winston";
 import { Envs } from "../../Envs";
 import * as immutable from 'immutable'
 import moment = require("moment");
-import { getS3Client, getTwitterClient } from "../../clients";
+import { getClientsFromEnvs } from "../../clients";
 import { sync } from "./sync";
-import { from, Observable } from 'rxjs';
-import fp from "lodash/fp";
+import { Observable } from 'rxjs';
 const sprintf = require("sprintf");
 
 let envs: immutable.Map<string, string | Environment | undefined>;
@@ -28,59 +26,18 @@ beforeEach(async () => {
 describe("sync function", () => {
     it("should sync user", async () => {
         const key = sprintf("%s_users.json", moment().format("YYYY-MM-DD-HH:mm:ss"))
-        const s3 = getS3Client({
-            accessKeyId: envs.get("S3_ACCESS_KEY_ID"),
-            secretAccessKey: envs.get("S3_SECRET_ACCESS_KEY"),
-        });
-
-        const tw = getTwitterClient({
-            consumer_key: envs.get("CONSUMER_API_KEY") as string,
-            consumer_secret: envs.get("CONSUMER_API_SECRET_KEY") as string,
-            access_token_key: envs.get("ACCESS_TOKEN") as string,
-            access_token_secret: envs.get("ACCESS_SECRET") as string,
-        });
+        const { s3, tw } = getClientsFromEnvs(envs)
 
         const result = sync(logger, tw, { Bucket: envs.get("S3_BUCKET_NAME") as string, Key: key }, s3)(validJSON());
-
-        // if (S.isLeft(result)) {
-
-        // }
-
-        // const r2 = S.either(fp.identity)(fp.identity)(result)
-
-        // // console.log(S.isRight(r))
-        // console.log(r2)
-
-        // const r: any = S.either(fp.identity)(fp.identity)(result)
-
-        // ((o: Observable<any>) => {
-        //     done();
-        //     return o;
-        //     return o.subscribe(
-        //         (r) => {
-        //             expect(r).toHaveProperty("ETag");
-        //             done();
-        //         },
-        //         err => {
-        //             logger.error(err);
-        //             done();
-        //         },
-        //         () => {
-        //             logger.info("completed", validJSON());
-        //             done();
-        //         },
-        //     );
-        // })
 
         if (result instanceof Observable) {
             try {
                 const p = await result.toPromise();
-                console.log("SUCCESS --->>", p)
             } catch (e) {
-                console.log('ERR UPLOAD ---->', e)
+                console.log(e)
             }
         } else {
-            console.log("ERROR ->>> ", result)
+            console.log(result)
         }
     });
 });
