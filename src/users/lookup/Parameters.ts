5f8b39ec9fccb9f63c3ec0fae22ff692134d989d
Jian Han <jianhan@mail.com>
2020-03-11 15:55:53 +1000

    Added docs and some dead code cleanup.

--- src/users/lookup/Parameters.ts
+++ src/users/lookup/Parameters.ts
@@ -1,24 +1,58 @@
 import { ArrayMaxSize, ArrayMinSize, ArrayUnique, IsArray, IsBoolean, IsNumber, IsString } from "class-validator";
 
+/**
+ * Parameters represents twitter api parameters when fetching users, it will be used
+ * for validations to ensure input parameters all valid before API call.
+ *
+ * @export
+ * @class Parameters
+ */
 export default class Parameters {
 
+    /**
+     * screen_name is screen_name parameter.
+     *
+     * @type string[]
+     * @memberof Parameters
+     */
     @IsArray()
     @ArrayMaxSize(100)
     @ArrayMinSize(1)
     @ArrayUnique()
     @IsString({ each: true })
+    // tslint:disable-next-line: variable-name
     public screen_name: string[] = [];
 
+    /**
+     * user_id is user_id parameter.
+     *
+     * @type number[]
+     * @memberof Parameters
+     */
     @IsArray()
     @ArrayMaxSize(100)
     @ArrayUnique()
     @IsNumber({}, { each: true })
+    // tslint:disable-next-line: variable-name
     public user_id: number[] = [];
 
+    /**
+     * include_entities is include_entities parameter.
+     *
+     * @type boolean
+     * @memberof Parameters
+     */
     @IsBoolean()
+    // tslint:disable-next-line: variable-name
     public include_entities: boolean = false;
 
+    /**
+     * tweet_mode is tweet_mode parameter
+     *
+     * @type boolean
+     * @memberof Parameters
+     */
     @IsBoolean()
+    // tslint:disable-next-line: variable-name
     public tweet_mode: boolean = false;
-
 }
--- src/users/lookup/sync.unit.test.ts
+++ src/users/lookup/sync.unit.test.ts
@@ -1,8 +1,4 @@
-import S from "sanctuary"
-import fp from "lodash/fp"
-import { from, Observable } from 'rxjs';
-
-describe("test sync func", () => {
+describe("sync function", () => {
     const validJSON = (): string => `{
         "screen_name": ["test1", "test1", "test2"],
         "user_id": [1, 2, 3],
@@ -10,44 +6,4 @@ describe("test sync func", () => {
         "tweet_mode": false
     }`;
 
-    const fetch = () => {
-        return new Promise((resolve, reject) => {
-            setTimeout(function () {
-                resolve("Success!")
-            }, 250)
-        }).then(x => x)
-    }
-
-    it("test2", async () => {
-        // const r = from(fetch()).subscribe(
-        //     x => x,
-        //     err => err,
-        //     () => console.log('complete')
-        // );
-
-        const r = await from(fetch()).toPromise();
-        console.log(r)
-    });
-
-    it("test", async () => {
-        const isOne = (n: number) => {
-            if (n === 1) {
-                return S.Right(1);
-            }
-
-            return S.Left(new Error("number is not 1"));
-        }
-
-        const multi = (n: number) => {
-            console.log("--->", n)
-            return n * 2;
-        }
-
-        const cp = S.pipe([isOne, S.map(multi)]);
-
-        const l = S.Left("left");
-
-        const r = S.Right("right");
-        console.log(S.fromEither(1)(l))
-    })
 });
