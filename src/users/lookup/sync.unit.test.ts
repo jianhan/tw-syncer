9c5b1efdfe6b87bc19067314cfc7922a8ba3cd8b
Jian Han <jianhan@mail.com>
2020-03-14 23:36:58 +1000

    Removed unwanted unit tests.

--- src/users/lookup/sync.unit.test.ts
+++ src/users/lookup/sync.unit.test.ts
@@ -7,7 +7,6 @@ import { ValidationError } from 'class-validator';
 import { Observable } from "rxjs"
 import { ManagedUpload } from 'aws-sdk/lib/s3/managed_upload';
 import _ from "lodash"
-import S from "sanctuary"
 
 const genJSON = (obj: { [key: string]: any } = {
     screen_name: ['test'],
@@ -146,10 +145,4 @@ describe("sync function", () => {
             expect(e.message).toBe(errMsg)
         }
     })
-
-    it("test", () => {
-        const arr = [];
-        console.log(S.ifElse((x: any[]) => _.size(x) > 0)(console.log)(console.log))
-    })
-
 });
