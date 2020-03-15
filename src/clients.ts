a836d486397e36897f4e474cb0e907d4f908a503
Jian Han <jianhan@mail.com>
2020-03-15 21:20:39 +1000

    Added docs to clients.ts

--- src/clients.ts
+++ src/clients.ts
@@ -5,10 +5,25 @@ import { ClientConfiguration } from "aws-sdk/clients/s3";
 import { Environment } from "jianhan-fp-lib";
 import * as immutable from "immutable";
 
+/**
+ * getTwitterClient function to get twitter client, used for compositions.
+ * 
+ * @param options 
+ */
 export const getTwitterClient = (options: AccessTokenOptions): Twitter => new Twitter(options);
 
+/**
+ * getS3Client function to get s3 client, used for function compositions.
+ * 
+ * @param configs 
+ */
 export const getS3Client = (configs: ClientConfiguration) => new S3(configs);
 
+/**
+ * getClientsFromEnvs is a simple method returns 2 clients by environment.
+ * 
+ * @param envs 
+ */
 export const getClientsFromEnvs = (envs: immutable.Map<string, string | Environment | undefined>) => {
     const s3 = getS3Client({
         accessKeyId: envs.get("S3_ACCESS_KEY_ID"),
