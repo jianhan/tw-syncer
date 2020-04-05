d10263db90a07bcfaeffce3951b62661bcffe58d
Jian Han <jianhan@mail.com>
2020-04-05 10:51:36 +1000

    Updated build script to create a folder for s3 syncing.

--- build-lambda.sh
+++ build-lambda.sh
@@ -15,9 +15,20 @@ echo $1 | grep -E -q '^v((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-
 npm run clean:lambda
 mkdir lambda
 cp -r dist/* lambda
-cd lambda || exit
+cd lambda || exit 1
 cp ../package.json .
 npm i --production
 rm *.json
 zip -r "lambda$1.zip" .
 rm -rf node_modules
+
+# delete all file but the zip within current folder
+mv *.zip ../
+rm -rf *
+mv ../*.zip .
+
+# create an extra folder just for the purpose of syncing folder to s3
+cd ..
+mkdir "$1"
+cd "$1" || exit 1
+cp ../lambda/"lambda$1.zip" .
