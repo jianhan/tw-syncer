5504ef464a4b3eed582a47ff3a43f16c648ac7f1
Jian Han <jianhan@mail.com>
2020-04-19 17:40:09 +1000

    feat(timeline): added sync function to timeline

---
+++ src/tweets/timeline/sync.ts
@@ -0,0 +1,19 @@
+import { of } from "rxjs";
+import _ from "lodash";
+import {Parameters, validateParameters} from "./Parameters"
+import {catchError, flatMap, map} from "rxjs/operators";
+import {envsMap} from "../../structures/envs";
+import {generateTimelineWithSinceId, fetch, fetchRequest, uploadRequest, upload} from "./s3";
+import {getLatestTimeline} from "./twitter";
+import Twitter = require("twitter");
+
+export const sync = (envs: envsMap, parameters: Parameters, s3: AWS.S3, twitter: Twitter) => of(parameters).pipe(
+    flatMap(validateParameters),
+    map(_.curry(fetchRequest)(envs)),
+    flatMap(_.curry(fetch)(s3)),
+    flatMap(generateTimelineWithSinceId),
+    flatMap(_.curry(getLatestTimeline)(twitter)(parameters)),
+    map(_.curry(uploadRequest)(envs)(parameters)),
+    flatMap(_.curry(upload)(s3)),
+    catchError(err => of(err))
+);
