7f162695b2c5d6d3550f471a910f67fbf0fc174f
Jian Han <jianhan@mail.com>
2020-04-20 22:33:11 +1000

    test(mergetimeline): fixed broken tests for mergeTimeline function

--- src/tweets/timeline/twitter.unit.test.ts
+++ src/tweets/timeline/twitter.unit.test.ts
@@ -11,7 +11,7 @@ describe("mergeTimeline function", () => {
                 "array nat", "array nat",
                 ((numArr1: number[], numArr2: number[]) => {
                     const merged = mergeTimeline(numArr2ObjArr(numArr1))(numArr2ObjArr(numArr2));
-                    const mergedIds = merged.tweets.map((t: any) => t.id);
+                    const mergedIds = merged.map((t: any) => t.id);
                     return _.difference(mergedIds, _.uniq(_.concat(numArr1, numArr2))).length === 0
                 })
             )
