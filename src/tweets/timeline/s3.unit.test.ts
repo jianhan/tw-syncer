e37b42aeccac45f205874a6c2048f982adfb597a
Jian Han <jianhan@mail.com>
2020-04-25 00:11:28 +1000

    test(s3): added unit test for fetch function in s3.ts

--- src/tweets/timeline/s3.unit.test.ts
+++ src/tweets/timeline/s3.unit.test.ts
@@ -1,7 +1,19 @@
-import {fetchRequest, getClient} from "./s3";
+import {fetchRequest, getClient, fetch} from "./s3";
 import AWS from "aws-sdk";
 import {toParameters} from "./Parameters";
 import {fileKey} from "../../operations";
+import { Logger } from "winston";
+import * as winston from "winston";
+
+const s3Client: AWS.S3 = new AWS.S3({accessKeyId: "test", secretAccessKey: "test"});
+const logger: Logger = winston.createLogger({
+    transports: [new winston.transports.Console()]
+}) as any;
+
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
 
 describe("getClient function", () => {
 
@@ -26,4 +38,20 @@ describe("fetchRequest function", () => {
         expect(r.Bucket).toEqual(bucket);
         expect(r.Key).toEqual(fileKey(nodeEnv, serviceName, "test", 'timeline'));
     })
+
+});
+
+describe("fetch function", () => {
+
+    it("should return empty body when promise reject", async () => {
+        // @ts-ignore
+        jest.spyOn(s3Client, "getObject").mockImplementation(() => ({promise: () => Promise.reject(new Error("test error"))}));
+        const spy = jest.spyOn(logger, "warn");
+
+        // @ts-ignore
+        const result = await fetch(s3Client)(logger)({}).toPromise();
+        expect(spy).toBeCalledTimes(1);
+
+        expect(result).toEqual({Body: ""});
+    });
 });
